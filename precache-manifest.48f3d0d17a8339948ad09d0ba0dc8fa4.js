3e2a8f86f856a3285b854f62ff78f05d077613e0
kevin puscan <kevinjpuscan@gmail.com>
2020-06-26 19:45:04 -0500

    Updates

---
+++ precache-manifest.48f3d0d17a8339948ad09d0ba0dc8fa4.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebaf89ef15b6bd0e9a8bcae45da87d23",
+    "url": "/personal-resume/index.html"
+  },
+  {
+    "revision": "209b4a10d53f3d9f331a",
+    "url": "/personal-resume/static/css/main.2d2d838b.chunk.css"
+  },
+  {
+    "revision": "fcd74f7a57ff68957d70",
+    "url": "/personal-resume/static/js/2.6f36379d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal-resume/static/js/2.6f36379d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209b4a10d53f3d9f331a",
+    "url": "/personal-resume/static/js/main.7872ac7f.chunk.js"
+  },
+  {
+    "revision": "30572a31ef7743eb0003",
+    "url": "/personal-resume/static/js/runtime-main.42f9558b.js"
+  },
+  {
+    "revision": "09cc9c615f0a0ff487e806647451f560",
+    "url": "/personal-resume/static/media/covid.09cc9c61.png"
+  },
+  {
+    "revision": "6205c31e3c440ae56df0fffa930eb1e1",
+    "url": "/personal-resume/static/media/gauge.6205c31e.png"
+  },
+  {
+    "revision": "4713b290d0d04bcf13e36a60ed905bd5",
+    "url": "/personal-resume/static/media/git.4713b290.png"
+  },
+  {
+    "revision": "c28b7341ba185807da8feedbbc634891",
+    "url": "/personal-resume/static/media/github.c28b7341.png"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/personal-resume/static/media/javascript.0e4fdce8.png"
+  },
+  {
+    "revision": "1b35d4fb3bcd3cf2e1e4455f72dea0fa",
+    "url": "/personal-resume/static/media/job-list.1b35d4fb.png"
+  },
+  {
+    "revision": "9da6821c3a70e940ad121f8596c1789b",
+    "url": "/personal-resume/static/media/kevin.9da6821c.jpg"
+  },
+  {
+    "revision": "f5b5d6f6c5f5de66213564aeff0d7151",
+    "url": "/personal-resume/static/media/laravel.f5b5d6f6.png"
+  },
+  {
+    "revision": "41cf718a6a7dbc32a27cbd9b7e6ebb87",
+    "url": "/personal-resume/static/media/leaflet.41cf718a.png"
+  },
+  {
+    "revision": "5bb9b3e2e5aee1812905b0328da00891",
+    "url": "/personal-resume/static/media/markers-map.5bb9b3e2.png"
+  },
+  {
+    "revision": "601ca06a1edbaef00d22cfcd3c63df24",
+    "url": "/personal-resume/static/media/nodejs.601ca06a.png"
+  },
+  {
+    "revision": "0a6c2c81ad5da0c723d3e617a6712213",
+    "url": "/personal-resume/static/media/okidoki.0a6c2c81.png"
+  },
+  {
+    "revision": "3d8db8c65af84e93a45d6d381d4a2aad",
+    "url": "/personal-resume/static/media/postgresql.3d8db8c6.png"
+  },
+  {
+    "revision": "071ed16fd425b8d86a4ac2c3a9a973d8",
+    "url": "/personal-resume/static/media/python.071ed16f.png"
+  },
+  {
+    "revision": "c94ed84dcfd1f69c1524e7b58173a3b5",
+    "url": "/personal-resume/static/media/react-shortly.c94ed84d.png"
+  },
+  {
+    "revision": "a7212ed4c02ca28a98c310a947c21998",
+    "url": "/personal-resume/static/media/reactjs.a7212ed4.png"
+  },
+  {
+    "revision": "bc4587cde1cc1dff96d8111fc305e85c",
+    "url": "/personal-resume/static/media/redux.bc4587cd.png"
+  },
+  {
+    "revision": "6d5b6422c8a1d5c2de82816aff0169a6",
+    "url": "/personal-resume/static/media/sass.6d5b6422.png"
+  },
+  {
+    "revision": "94e9f4fd2dda02c3d47d7f056367776e",
+    "url": "/personal-resume/static/media/styledcomponents.94e9f4fd.png"
+  },
+  {
+    "revision": "e3d2401526d185059b49889a595d5a23",
+    "url": "/personal-resume/static/media/ubuntu.e3d24015.png"
+  },
+  {
+    "revision": "430af113a58174167f33f8b6e522c515",
+    "url": "/personal-resume/static/media/vuejs.430af113.png"
+  },
+  {
+    "revision": "a48fad9786175f354306569feb6bf58f",
+    "url": "/personal-resume/static/media/webrtc.a48fad97.png"
+  }
+]);
